2728265d563ca13eb90e29a28cd664e34cbbb335
Eric Horvat <eric.nahuel.horvat@gmail.com>
2018-10-09 21:34:04 -0300

    Draw distance

--- src/main/java/ar/edu/itba/montu/visual/CastleProcessingVisualAgent.java
+++ src/main/java/ar/edu/itba/montu/visual/CastleProcessingVisualAgent.java
@@ -1,6 +1,7 @@
 package ar.edu.itba.montu.visual;
 
 import ar.edu.itba.montu.abstraction.LocatableAgent;
+import ar.edu.itba.montu.war.castle.Castle;
 
 import java.util.UUID;
 
@@ -15,5 +16,13 @@ import java.util.UUID;
   void draw(ProcessingApplet applet) {
     super.draw(applet);
     applet.rect(x-R,y-R,2*R,2*R);
+    applet.noFill();
+    float viewDistance = (float) ((Castle)locatableAgent).characteristics().viewDistance();
+    applet.stroke(applet.color(120,100,100));
+    applet.ellipse(x-viewDistance,y-viewDistance,2*viewDistance,2*viewDistance);
+    float attackDistance = (float) ((Castle)locatableAgent).characteristics().attackDistance();
+    applet.stroke(applet.color(0,100,100));
+    applet.ellipse(x-attackDistance,y-attackDistance,2*attackDistance,2*attackDistance);
+    applet.noStroke();
   }
 }
