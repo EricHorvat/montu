3222659529ec861ad33dfa3a86a01ec04c435f02
Eric Horvat <eric.nahuel.horvat@gmail.com>
2018-11-04 21:00:44 -0300

    [TEST] Changing dividers

--- src/main/java/ar/edu/itba/montu/war/people/WarriorCharacteristics.java
+++ src/main/java/ar/edu/itba/montu/war/people/WarriorCharacteristics.java
@@ -20,25 +20,25 @@ public class WarriorCharacteristics extends AttackingAgentCharacteristics {
 				LocatableAgentCharacteristics.withViewDistanceAndHealthPoints(
 						// Warriors get 20% of the castles view distance initially
 						// Of course, a normal distribution applies when born
-						Math.max(RandomUtil.getNormalDistribution(c.characteristics().viewDistance() / 5.0, 0.1), 0.0),
+						Math.max(RandomUtil.getNormalDistribution(c.characteristics().viewDistance() / 3.0, 0.1), 0.0),
 						// Warriors get 10% of the castles health points initially
 						// Of course, a normal distribution applies when born
 						(int)RandomUtil.getNormalDistribution(c.characteristics().maxHealthPoints() / 10.0, 50.0)
 				),
 				// Warriors get 10% of the castles attack distance initially
 				// Of course, a normal distribution applies when born
-				Math.max(RandomUtil.getNormalDistribution(c.characteristics().attackDistance() / 10.0, 0.05), 0.0),
+				Math.max(RandomUtil.getNormalDistribution(c.characteristics().attackDistance() / 6.0, 0.05), 0.0),
 				// A castles attack harm is (100 - offense capacity) of the kingdom
 				// This means the castles best attack is his defense = (100 - offense)
 				// Therefore, we (100 - attack harm) and get the 10% of it
 				// Warriors get 10% of (100 - castles attack harm) initially
 				// Of course, a normal distribution applies when born
-				(int)Math.max(RandomUtil.getNormalDistribution((100.0 - c.characteristics().attackHarm()) / 10.0, 2), 0.0)
+				(int)Math.max(RandomUtil.getNormalDistribution((100.0 - c.characteristics().attackHarm()) / 100.0, 2), 0.0)
 		);
 		// Speed is in km/min. These come near to 0.48km/min
 		// We apply a noise of 10% that
 		this.speed = Characteristic.withFixedValue(
-				Math.max(RandomUtil.getNormalDistribution(c.kingdom().characteristics().warriorSpeed(), 0.05), 0.0)
+				Math.max(RandomUtil.getNormalDistribution(c.kingdom().characteristics().warriorSpeed(), 0.05), 1E-5)
 		);
 	}
 	
